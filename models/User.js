3cd42111b3672ad887d22c5845913d00d2dc49d2
Quang Hai <haimobiarmy2002@gmail.com>
2023-09-21 01:27:32 +0700

    kk

--- models/User.js
+++ models/User.js
@@ -5,12 +5,9 @@ const ObjectId = Schema.ObjectId;
 
 const User = mongoose.model('User',
     new Schema({
-
-
         username: {
             type: String,
             required: true,
-
         },
         password: {
             type: String,
